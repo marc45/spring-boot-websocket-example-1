ca81310ba750528f47f1487d652093bcfbc9d55e
肖泽锋 <qq523107430@163.com>
2017-12-30 01:20:08 +0800

    简单聊天室

--- src/main/java/com/gz/spring/boot/websocket/example/configuration/WebSocketConfig.java
+++ src/main/java/com/gz/spring/boot/websocket/example/configuration/WebSocketConfig.java
@@ -6,27 +6,32 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-/** 配置WebSocket
- * @EnableWebSocketMessageBroker 开启使用 STOMP 协议来传输基于代理 message broker 的消息
+/**
+ * 配置WebSocket
+ *
  * @author xiaozefeng
+ * @EnableWebSocketMessageBroker 开启使用 STOMP 协议来传输基于代理 message broker 的消息
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 注册一个STOMP 的 endpoint 并支持SockJS协议
         registry.addEndpoint("endpointSteven").withSockJS();
+        registry.addEndpoint("endpointChat").withSockJS();
     }
 
     /**
      * 配置消息代理
+     *
      * @param registry
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 广播式配置一个/topic消息代理
-        registry.enableSimpleBroker("/topic");
+        // 广播式配置一个/queue消息代理
+        registry.enableSimpleBroker("/queue", "/topic");
     }
 }
--- src/main/java/com/gz/spring/boot/websocket/example/web/WsController.java
+++ src/main/java/com/gz/spring/boot/websocket/example/web/WsController.java
@@ -2,15 +2,21 @@ package com.gz.spring.boot.websocket.example.web;
 
 import com.gz.spring.boot.websocket.example.msssage.StevenMessage;
 import com.gz.spring.boot.websocket.example.msssage.StevenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 /**
  * @author xiaozefeng
  */
 @Controller
 public class WsController {
+    @Autowired
+    private SimpMessageSendingOperations simpMessageSendingOperations;
 
     /**
      * @MessageMapping 浏览器向服务端发送请求时，映射到/welcome 这个地址
@@ -20,10 +26,23 @@ public class WsController {
      * @return
      * @throws InterruptedException
      */
-    @MessageMapping("welcome")
+    @MessageMapping("/welcome")
     @SendTo("/topic/getResponse")
     public StevenResponse say(StevenMessage message) throws InterruptedException {
         Thread.sleep(3000);
         return new StevenResponse("Welcome, " + message.getName() + "!");
     }
+
+    @MessageMapping("/chat")
+    public void handleChat(Principal principal, String msg) {
+        if (principal.getName().equals("wyf")) {
+            simpMessageSendingOperations.convertAndSendToUser("wisely",
+                    "/queue/notifications",
+                    principal.getName() + "-send:" + msg);
+        } else {
+            simpMessageSendingOperations.convertAndSendToUser("wyf", "/queue/notifications", principal.getName() + "-send:" + msg);
+        }
+    }
+
+
 }
